da163abf1dcdf132d45bfa88ef4e4cf7dbecbe3f
ay27 <me@ay27.pw>
2017-01-15 20:39:44 +0800

    adjust test file

--- src/ops.py
+++ src/ops.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 
 
-def __gen_perm(order, mode):
+def _gen_perm(order, mode):
     """
     Generate the specified permutation by the given mode
     """
@@ -20,19 +20,19 @@ def unfold(tensor, mode=0):
     :param mode: int, default is 0
     :return: tf.Tensor
     """
-    perm = __gen_perm(tensor.get_shape().ndims, mode)
+    perm = _gen_perm(tensor.get_shape().ndims, mode)
     return tf.reshape(tf.transpose(tensor, perm), (tensor.get_shape().as_list()[mode], -1))
 
 
 def fold(unfolded_tensor, mode, shape):
     """
     Fold an unfolded tensor to tensor with specified shape
-    :param unfolded_tensor: tf.Tensor, ndarray
+    :param unfolded_tensor: tf.Tensor
     :param mode: int
     :param shape: the specified shape of target tensor
     :return: tf.Tensor
     """
-    perm = __gen_perm(len(shape), mode)
+    perm = _gen_perm(len(shape), mode)
     shape_now = [shape[_] for _ in perm]
     back_perm = [item[0] for item in sorted(enumerate(perm), key=lambda x: x[1])]
     return tf.transpose(tf.reshape(unfolded_tensor, shape_now), back_perm)
@@ -41,32 +41,32 @@ def fold(unfolded_tensor, mode, shape):
 def t2mat(tensor, r_axis, c_axis):
     """
     Transfer a tensor to a matrix by given row axis and column axis
-    :param tensor: tf.Tensor, ndarray
+    :param tensor: tf.Tensor
     :param r_axis: int, list
     :param c_axis: int, list
     :return: matrix-like tf.Tensor
     """
     if isinstance(r_axis, int):
         indies = [r_axis]
-        row_size = tensor.shape[r_axis]
+        row_size = tensor.get_shape()[r_axis].value
     else:
         indies = r_axis
-        row_size = np.prod([tensor.shape[i] for i in r_axis])
+        row_size = np.prod([tensor.get_shape()[i].value for i in r_axis])
+    if c_axis == -1:
+        c_axis = [_ for _ in range(tensor.get_shape().ndims) if _ not in r_axis]
     if isinstance(c_axis, int):
-        if c_axis == -1:
-            c_axis = [_ for _ in range(len(tensor.shape)) if _ not in r_axis]
         indies.append(c_axis)
-        col_size = tensor.shape[c_axis]
+        col_size = tensor.get_shape()[c_axis].value
     else:
         indies = indies + c_axis
-        col_size = np.prod([tensor.shape[i] for i in c_axis])
+        col_size = np.prod([tensor.get_shape()[i].value for i in c_axis])
     return tf.reshape(tf.transpose(tensor, indies), (int(row_size), int(col_size)))
 
 
 def vectorize(tensor):
     """
     Verctorize a tensor to a vector
-    :param tensor: tf.Tensor, ndarray
+    :param tensor: tf.Tensor
     :return: vector-like tf.Tensor
     """
     return tf.reshape(tensor, [-1])
@@ -84,32 +84,48 @@ def vec_to_tensor(vec, shape):
 
 def mul(tensorA, tensorB, a_axis, b_axis):
     """
-
-    :param tensorA:
-    :param tensorB:
-    :param a_axis:
-    :param b_axis:
-    :return:
+    Multiple tensor A and tensor B by the axis of a_axis and b_axis
+    :param tensorA: tf.Tensor
+    :param tensorB: tf.Tensor
+    :param a_axis: list
+    :param b_axis: list
+    :return: tf.Tensor
     """
     A = t2mat(tensorA, a_axis, -1)
     B = t2mat(tensorB, b_axis, -1)
-    mat_dot = tf.matmul(A, B, transpose_b=True)
-    back_shape = [_ for _ in range(len(tensorA.shape)) if _ not in a_axis] + \
-                 [_ for _ in range(len(tensorB.shape)) if _ not in b_axis]
+    mat_dot = tf.matmul(A, B, transpose_a=True)
+    back_shape = [tensorA.get_shape()[_].value for _ in range(tensorA.get_shape().ndims) if _ not in a_axis] + \
+                 [tensorB.get_shape()[_].value for _ in range(tensorB.get_shape().ndims) if _ not in b_axis]
     return tf.reshape(mat_dot, back_shape)
 
 
 def inner(tensorA, tensorB):
     """
-
-    :param tensorA:
-    :param tensorB:
-    :return:
+    Inner product or tensor A and tensor B. The shape of A and B must be equal.
+    :param tensorA: tf.Tensor
+    :param tensorB: tf.Tensor
+    :return: constant-like tf.Tensor
+    :raise: ValueError
+    raise if the shape of A and B not equal
     """
+    if tensorA.get_shape() != tensorB.get_shape():
+        raise ValueError('the shape of tensor A and B must be equal')
     return tf.reduce_sum(vectorize(tensorA) * vectorize(tensorB))
 
 
 def hadamard(matrices, skip_matrices_index=None, reverse=False):
+    """
+    Hadamard product of given matrices, which is the element product of matrix.
+    :param matrices: List
+
+    :param skip_matrices_index: List
+    skip some matrices
+
+    :param reverse: bool
+    reverse the matrices order
+
+    :return: tf.Tensor
+    """
     if skip_matrices_index is not None:
         matrices = [matrices[_] for _ in range(len(matrices)) if _ not in skip_matrices_index]
     if reverse:
@@ -122,18 +138,21 @@ def hadamard(matrices, skip_matrices_index=None, reverse=False):
 
 def kron(matrices, skip_matrices_index=None, reverse=False):
     """
+    Kronecker product of given matrices.
+    :param matrices: List
 
-    :param matrices:
-    :param skip_matrices_index:
-    :param reverse:
-    :return:
+    :param skip_matrices_index: List
+
+    :param reverse: bool
+
+    :return: tf.Tensor
     """
     if skip_matrices_index is not None:
         matrices = [matrices[_] for _ in range(len(matrices)) if _ not in skip_matrices_index]
     if reverse:
         matrices = matrices[::-1]
     start = ord('a')
-    source = ','.join(chr(start + i) + chr(start + i + 1) for i in range(0, 2*len(matrices), 2))
+    source = ','.join(chr(start + i) + chr(start + i + 1) for i in range(0, 2 * len(matrices), 2))
     row = ''.join(chr(start + i) for i in range(0, len(matrices), 2))
     col = ''.join(chr(start + i) for i in range(1, len(matrices), 2))
     operation = source + '->' + row + col
@@ -146,11 +165,14 @@ def kron(matrices, skip_matrices_index=None, reverse=False):
 
 def khatri(matrices, skip_matrices_index=None, reverse=False):
     """
+    Khatri-Rao product
+    :param matrices: List
 
-    :param matrices:
-    :param skip_matrices_index:
-    :param reverse:
-    :return:
+    :param skip_matrices_index: List
+
+    :param reverse: bool
+
+    :return: tf.Tensor
     """
     if skip_matrices_index is not None:
         matrices = [matrices[_] for _ in range(len(matrices)) if _ not in skip_matrices_index]
--- test/ops_test.py
+++ test/ops_test.py
@@ -6,86 +6,81 @@ import src.ops as ops
 
 
 class MyTestCase(unittest.TestCase):
-   def setUp(self):
-        self.tmpcase1 = [[[1, 13], [4, 16], [7, 19], [10, 22]],
-                        [[2, 14], [5, 17], [8, 20], [11, 23]],
-                        [[3, 15], [6, 18], [9, 21], [12, 24]]]
-        self.tt = 0
+    def setUp(self):
+        # shape of tmp 3x4x2
+        self.tmp = [[[1, 13], [4, 16], [7, 19], [10, 22]],
+                    [[2, 14], [5, 17], [8, 20], [11, 23]],
+                    [[3, 15], [6, 18], [9, 21], [12, 24]]]
+        self.np_x = np.array(self.tmp)
+        self.tf_x = tf.constant(self.np_x)
 
-        self.vectorcase1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
-                            12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]
+    def test_gen_perm(self):
+        x = [2, 3, 1, 0]
+        res = ops._gen_perm(4, 2)
+        np.testing.assert_array_equal(x, res)
 
-        self.mc1 = np.random.rand(7, 8)
-        self.mc2 = np.random.rand(100, 200)
-        self.mc3 = np.random.rand(100, 200)
-        self.matrices = [self.mc2, self.mc3]
-        self.tmp = 0
-
-   def test_unfold(self):
-        # 3x4x2 tensor
-        x = np.array(self.tmpcase1)
-
-        r1 = np.reshape(np.transpose(self.tmpcase1, [1, 2, 0]), [4, 6])
+    def test_unfold(self):
+        # mode 1: 3x4x2 -> 4x2x3 -> 4x6
+        r1 = np.reshape(np.transpose(self.np_x, [1, 2, 0]), [4, 6])
         with tf.Session().as_default():
-            r2 = ops.unfold(x, 1).eval()
+            r2 = ops.unfold(self.tf_x, 1).eval()
         np.testing.assert_array_almost_equal(r1, r2)
 
-   def test_fold(self):
-        x = np.array(self.tmpcase1)
+    def test_fold(self):
+        # mode 2 : 3x4x2 -> 2x4x3 -> 2x12
+        mode_2_mat = np.reshape(np.transpose(self.np_x, [2, 1, 0]), [2, 12])
+        mode_2_tf = tf.constant(mode_2_mat)
         with tf.Session().as_default():
-            r = ops.fold(ops.unfold(x, 1), 1, (3, 4, 2)).eval()
-        np.testing.assert_array_almost_equal(x, r)
+            res = ops.fold(mode_2_tf, 2, [3, 4, 2]).eval()
+        np.testing.assert_array_almost_equal(self.np_x, res)
 
+    def test_t2mat(self):
+        np_A = np.random.rand(2, 3, 4, 5)
+        tf_A = tf.constant(np_A)
 
-    # n
-
-   def test_t2mat(self):
-        x = np.array(self.tmpcase1)
-        r1 = np.reshape(np.transpose(self.tmpcase1, [1,2,0]),[4, 6])
-        # set axis value 1.
-        axis = 0
-        matrix = np.array(self.tmpcase1)
-        indices = list(range(len(matrix.shape)))
-        indices[0], indices[axis] = indices[axis], indices[0]
-        tmp = ops.matricization(axis, indices[1:])
-        tmp = np.dot(matrix, tmp)
-        # tmp = np.array(tmp)
-        back_shape = [self.shape[_] for _ in indices]
-        back_shape[0] = tmp.shape[0]
-        np.reshape(tmp, back_shape).transpose(indices)
+        res1 = np.reshape(np.transpose(np_A, [1, 2, 3, 0]), [12, 10])
         with tf.Session().as_default():
-            r2 = ops.t2mat(x, 1, [0, 2]).eval()
-        np.testing.assert_array_almost_equal(r1, r2)
+            res2 = ops.t2mat(tf_A, [1, 2], [3, 0]).eval()
+        np.testing.assert_array_almost_equal(res1, res2)
 
-   def test_vectorize(self):
-        x = np.array(self.tmpcase1)
-        r1 = x.reshape(-1)
+    def test_vectorize(self):
+        res1 = np.reshape(self.np_x, -1)
 
         with tf.Session().as_default():
-            r2 = ops.vectorize(x).eval()
-        np.testing.assert_array_equal(r1, r2)
+            res2 = ops.vectorize(self.tf_x).eval()
+        np.testing.assert_array_equal(res1, res2)
 
-   def test_vec_to_tensor(self):
-        x = np.array(self.vectorcase1)
-        r1 = np.reshape(x, (3, 4, 2))
+    def test_vec_to_tensor(self):
+        np_vec = np.reshape(self.np_x, -1)
+        tf_vec = tf.constant(np_vec)
         with tf.Session().as_default():
-            r2 = ops.vec_to_tensor(x, (3, 4, 2))
+            res = ops.vec_to_tensor(tf_vec, (3, 4, 2)).eval()
+        np.testing.assert_array_almost_equal(self.np_x, res)
 
-   def test_dot(self):
-        pass
+    def test_mul(self):
+        np_A = np.random.rand(2, 3, 4)
+        np_B = np.random.rand(4, 3, 6)
+        np_res = np.einsum('ijk,klm->ijlm', np_A, np_B)
 
-
-   def test_inner(self):
-        r1 = 0
-        x1 = self.mc2.reshape(-1)
-        x2 = self.mc3.reshape(-1)
-        for i in range(len(x1)):
-            r1 = r1 + x1[i] * x2[i]
+        tf_A = tf.constant(np_A)
+        tf_B = tf.constant(np_B)
         with tf.Session().as_default():
-            r2 = ops.inner(self.mc2, self.mc3).eval()
-        np.testing.assert_array_almost_equal(r1, r2)
+            tf_res = ops.mul(tf_A, tf_B, [2], [0]).eval()
+        self.assertEqual(len(np_res.shape), 4)
+        np.testing.assert_array_almost_equal(np_res, tf_res)
 
-   def test_kron(self):
+    def test_inner(self):
+        np_A = np.random.rand(2, 3, 4)
+        np_B = np.random.rand(2, 3, 4)
+        np_res = np.sum(np.reshape(np_A, -1) * np.reshape(np_B, -1))
+
+        tf_A = tf.constant(np_A)
+        tf_B = tf.constant(np_B)
+        with tf.Session().as_default():
+            tf_res = ops.inner(tf_A, tf_B).eval()
+        np.testing.assert_almost_equal(np_res, tf_res)
+
+    def test_kron(self):
         a = np.kron(self.mc2, self.mc3)
         b = np.kron(self.mc3, self.mc2)
         with tf.Session().as_default():
@@ -94,7 +89,7 @@ class MyTestCase(unittest.TestCase):
         np.testing.assert_array_almost_equal(a, r1)
         np.testing.assert_array_almost_equal(b, r2)
 
-   def test_khatri(self):
+    def test_khatri(self):
         a = np.random.rand(3, 4)
         b = np.random.rand(5, 4)
         c = np.random.rand(6, 4)
@@ -102,7 +97,6 @@ class MyTestCase(unittest.TestCase):
             res = ops.khatri([a, b, c]).eval()
         tmp = np.einsum('az,bz,cz->abcz', a, b, c).reshape(-1, 4)
         np.testing.assert_array_almost_equal(res, tmp)
-
 
 
 if __name__ == '__main__':
