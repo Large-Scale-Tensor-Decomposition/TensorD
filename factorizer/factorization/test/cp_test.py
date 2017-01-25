2d0250d094def446bfe10eee63db31ea17bb311f
ay27 <me@ay27.pw>
2017-01-25 17:49:15 +0800

    backup

--- factorizer/factorization/test/cp_test.py
+++ factorizer/factorization/test/cp_test.py
@@ -7,8 +7,8 @@ import tensorflow as tf
 import logging
 from factorizer.factorization.cp import cp
 from numpy.random import rand
-assert_array_equal = np.testing.assert_array_almost_equal
 
+assert_array_equal = np.testing.assert_array_almost_equal
 
 logger = logging.getLogger('TEST')
 
