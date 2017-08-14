929864f62e7a75c73ee729f9066a27faef47df58
Shiloh Leung <zszx0259@me.com>
2017-08-14 15:41:04 +0800

    finish reader, use tf.SparseTensor & tf.sparse_tensor_to_dense

--- tensorD/dataproc/reader.py
+++ tensorD/dataproc/reader.py
@@ -29,7 +29,7 @@ class TensorReader(object):
         self._full_data = None
         self._type = self._file_path.split('.')[-1]
 
-    def read_demo1(self):
+    def read(self):
         file = open(self._file_path, 'r')
         str_in = []
         if self._type == 'csv' or self._type == 'txt':
@@ -49,76 +49,9 @@ class TensorReader(object):
             idx[row] = np.array([int(entry[mode]) for mode in range(order)])
             value[row] = float(entry[-1])
         max_dim = np.max(idx, axis=0) + np.ones(order).astype(int)
-        self._sparse_data = tf.SparseTensor(indices=idx, values=value, dense_shape=max_dim)
-        self._full_data = tf.sparse_tensor_to_dense(self._sparse_data)
-
-    def read_demo2(self):
-        tmp = np.loadtxt(self._file_path, dtype=float, delimiter=',')
-        order = len(tmp[0]) - 1
-        max_dim = np.max(tmp[:, 0:order], axis=0).astype(int) + np.ones(order).astype(int)
-        self._sparse_data = tf.SparseTensor(indices=tmp[:, 0:order].astype(int), values=tmp[:, order],
-                                            dense_shape=max_dim)
-        self._full_data = tf.sparse_tensor_to_dense(self._sparse_data)
-
-    def read_demo3(self):
-        file = open(self._file_path, 'r')
-        str_in = []
-        if self._type == 'csv' or self._type == 'txt':
-            for row in csv.reader(file):
-                if len(row) != 0:
-                    str_in.append(row)
-        else:
-            raise ArgumentError(self._type + ' file is not supported by TensorReader.')
-        file.close()
-
-        idx = []
-        value = []
-        order = len(str_in[0]) - 1
-        max_dim = [0 for _ in range(order)]
-
-        for entry in str_in:
-            tmp_idx = [int(entry[mode]) for mode in range(order)]
-            idx.append(tmp_idx)
-            value.append(float(entry[-1]))
-            # TODO : assume that index starts from zero ? can be selected later
-            for mode in range(order):
-                if max_dim[mode] < tmp_idx[mode] + 1:
-                    max_dim[mode] = tmp_idx[mode] + 1
 
         self._sparse_data = tf.SparseTensor(indices=idx, values=value, dense_shape=max_dim)
-        self._full_data = tf.sparse_tensor_to_dense(self._sparse_data)
-
-    def read_demo4(self):
-        file = open(self._file_path, 'r')
-        str_in = []
-        if self._type == 'csv' or self._type == 'txt':
-            for row in csv.reader(file):
-                if len(row) != 0:
-                    str_in.append(row)
-        else:
-            raise ArgumentError(self._type + ' file is not supported by TensorReader.')
-
-        file.close()
-
-        order = len(str_in[0]) - 1
-        sparse_data = dict()
-        max_dim = [0 for _ in range(order)]
-
-        for entry in str_in:
-            idx_tuple = tuple([int(entry[mode]) for mode in range(order)])
-            sparse_data[idx_tuple] = float(entry[-1])
-
-            # TODO : assume that index starts from zero ? can be selected later
-            for mode in range(order):
-                if max_dim[mode] < idx_tuple[mode] + 1:
-                    max_dim[mode] = idx_tuple[mode] + 1
-
-        full_data = np.zeros(shape=max_dim)
-        for entry in sparse_data:
-            full_data[entry] = sparse_data[entry]
-
-        self._sparse_data = sparse_data
-        self._full_data = full_data
+        self._full_data = tf.sparse_tensor_to_dense(self._sparse_data, validate_indices=False)
 
     @property
     def full_data(self):
@@ -127,43 +60,3 @@ class TensorReader(object):
     @property
     def sparse_data(self):
         return self._sparse_data
-
-#
-# class TensorReader(object):
-#     """
-#
-#     Attributes
-#     ----------
-#     file: opening file with given file_path
-#
-#     """
-#
-#     def __init__(self, file_path, fmt='{:d} {:d} {:d} {:f}', encoding=None):
-#         """
-#
-#         Parameters
-#         ----------
-#         file_path: file path
-#         fmt: line format of data, see parse package_ for more details
-#         encoding: file encoding
-#
-#         .. _parse package: https://pypi.python.org/pypi/parse
-#         """
-#         self.file = open(file_path, mode='r', encoding=encoding)
-#         self.fmt = parse.compile(fmt)
-#
-#     def next(self):
-#         """
-#         Get the next line data.
-#
-#         Yields
-#         ------
-#         list
-#             list of one row data
-#         """
-#         for line in self.file:
-#             if not line:
-#                 continue
-#             items = list(self.fmt.parse(line))
-#             if items:
-#                 yield items
--- tensorD/test/reader_test.py
+++ tensorD/test/reader_test.py
@@ -8,31 +8,17 @@
 
 from tensorD.dataproc.reader import TensorReader
 import time
+import tensorflow as tf
 
 if __name__ == '__main__':
     print('csv file:')
     file_path = 'data1.csv'
 
-    treader1 = TensorReader(file_path)
-    start1 = time.time()
-    treader1.read_demo1()
-    end1 = time.time()
-    print('read demo 1 time: %.6f s\n' % (end1 - start1))
-
-    treader2 = TensorReader(file_path)
-    start2 = time.time()
-    treader2.read_demo2()
-    end2 = time.time()
-    print('read demo 2 time: %.6f s\n' % (end2 - start2))
-
-    treader3 = TensorReader(file_path)
-    start3 = time.time()
-    treader3.read_demo3()
-    end3 = time.time()
-    print('read demo 3 time: %.6f s\n' % (end3 - start3))
-
-    treader4 = TensorReader(file_path)
-    start4 = time.time()
-    treader4.read_demo4()
-    end4 = time.time()
-    print('read demo 4 time: %.6f s\n' % (end4 - start4))
+    treader = TensorReader(file_path)
+    start = time.time()
+    treader.read()
+    end = time.time()
+    print('reader time: %.6f s\n' % (end - start))
+    with tf.Session() as sess:
+        print(sess.run(treader.sparse_data))
+        print(sess.run(treader.full_data))
