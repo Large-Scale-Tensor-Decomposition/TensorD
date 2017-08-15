7111827b6e4634323837f2e773e57d324617eb68
Shiloh Leung <zszx0259@me.com>
2017-08-15 23:25:51 +0800

    add non-negative files

---
+++ tensorD/factorization/test/ncp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/15 PM10:53
+# @Author  : Shiloh Leung
+# @Site    : 
+# @File    : ncp.py
+# @Software: PyCharm Community Edition
---
+++ tensorD/factorization/test/ntucker.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/15 PM10:54
+# @Author  : Shiloh Leung
+# @Site    : 
+# @File    : ntucker.py
+# @Software: PyCharm Community Edition
