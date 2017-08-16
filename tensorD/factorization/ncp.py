cc24418bbe1c7c50056d66ed7e1dc23138e91e7e
Shiloh Leung <zszx0259@me.com>
2017-08-16 11:49:21 +0800

    add ncp & ntucker files in factorization

---
+++ tensorD/factorization/ncp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/16 AM11:48
+# @Author  : Shiloh Leung
+# @Site    : 
+# @File    : ncp.py
+# @Software: PyCharm Community Edition
---
+++ tensorD/factorization/ntucker.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/8/16 AM11:48
+# @Author  : Shiloh Leung
+# @Site    : 
+# @File    : ntucker.py
+# @Software: PyCharm Community Edition
+
