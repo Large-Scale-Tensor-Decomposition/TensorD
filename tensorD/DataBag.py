961203332f80162e35e6faf6ba284e712c92991c
Shiloh Leung <zszx0259@me.com>
2017-09-12 01:08:09 +0800

    ncp build_model not finished yet

---
+++ tensorD/DataBag.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/7/31 PM2:36
+# @Author  : Shiloh Leung
+# @Site    :
+# @File    : DataBag.py
+# @Software: PyCharm Community Edition
+import tensorflow as tf
+import numpy as np
+from tensorD.base.type import *
+
+def primesfrom3to(n):
+    """ Returns a array of primes, 3 <= p < n """
+    sieve = np.ones(int(n/2), dtype=np.bool)
+    for i in range(3,int(n**0.5)+1,2):
+        if sieve[int(i/2)]:
+            sieve[int(i*i/2)::i] = False
+    return 2*np.nonzero(sieve)[0][1::]+1
+
+
+
+def gen_core(coreR, number_list):
+    """
+    generate core tensor of shape R \\times R \\times R
+    generate algrithm:
+    return:
+       tf.tensor of shape R \\times R \\times R
+    """
+    if coreR % 2 == 0:
+        assert coreR*coreR/2 + coreR < len(number_list), "number list is not long enough"
+    else:
+        assert (coreR+1)*(coreR+1)/2 + coreR < len(number_list), "number list is not long enough"
+    core = np.zeros(shape=(coreR, coreR, coreR))
+    count = 0
+    for dim1 in range(coreR):
+        for dim2 in range(coreR):
+            if dim1 % 2 == 0:
+                row = number_list[count:count + coreR]
+                if dim2 % 2 == 0:
+                    core[:,dim2,dim1] = np.array(row)
+                    count += 2
+                else:
+                    core[:,dim2,dim1] = np.array(row[::-1])
+            else:
+                core[:,:,dim1] = np.transpose(core[:,:,dim1-1])
+    return core
+
+
+
+
+def gen_ABC(I_list,R,number_list):
+    """
+    :param I_list: list of 3 int
+    :param R: int, the shapes of A,B,C is I_i \\times R
+    :param number_list: to choose from, list or np.ndarray
+    :return:
+      a list of 3 matrices, each shape is I_i \\times R
+    """
+    if isinstance(number_list, list):
+        number_list = np.array(number_list)
+    max_I = max(I_list)
+    if max_I % 2 == 0:
+        assert max_I/2 + R < len(number_list), "number list is not long enough"
+    else:
+        assert (max_I+1)/2 + R < len(number_list), "number list is not long enough"
+    ABC = []
+    for i in range(len(I_list)):
+        count = 0
+        ABC.append(np.zeros(shape=(I_list[i],R)))
+        for row in range(I_list[i]):
+            if row % 2 == 0:
+                line = number_list[count:count + R]
+                ABC[i][row,:] = line
+                count += 1
+            else:
+                ABC[i][row,:] = line[::-1]
+    return ABC
+
+
+def gen_test_tensor(I_list, R, max_prime=10000):
+    # the default number list has 1228 prime numbers, enough for R=48
+    # but notice that max(I_list)/2 + R should be less than 1228
+    number_list = primesfrom3to(max_prime)
+    ABC = gen_ABC(I_list, R, number_list)
+    core = gen_core(R, number_list)
+    P = TTensor(core, ABC)
+    with tf.Session() as sess:
+        full_tensor = sess.run(P.extract())
+    return full_tensor
+
+
+
+def rand_list(shape_list, rank):
+    # initial seed
+    seed = 11796
+    order = len(shape_list)
+    init_mat = [np.zeros(shape=(shape_list[ii], rank)) for ii in range(order)]
+    for mode in range(order):
+        if mode != 0:
+            I_i = shape_list[mode]
+            for ii in range(I_i):
+                for jj in range(rank):
+                    seed = (8121*seed + 28411) % 1334456
+                    init_mat[mode][ii,jj] = seed / 1334456 * 2
+
+    return init_mat
+
+
+
+def insert_test(tf_variable):
+    init_op = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init_op)
+        print(sess.run(tf_variable))
--- tensorD/factorization/ncp.py
+++ tensorD/factorization/ncp.py
@@ -8,10 +8,104 @@
 
 import tensorflow as tf
 import numpy as np
+from numpy.random import rand
 from tensorD.loss import *
+from tensorD.DataBag import *
 from tensorD.base import *
 from .factorization import *
 from .env import *
+
+class NCP(BaseFact):
+    class NCP_Args(object):
+        def __init__(self, rank, validation_internal=-1, verbose=False):
+            self.rank = rank
+            self.validation_internal = validation_internal
+            self.verbose = verbose
+
+    def __init__(self, env):
+        assert isinstance(env, Environment)
+        self._env = env
+        self._model = None
+        self._full_tensor = None
+        self._factors = None
+        self._args = None
+        self._init_op = None
+        self._factor_update_op = None
+        self._full_op = None
+        self._loss_op = None
+        self._is_train_finish = False
+
+    def build_model(self, args):
+        assert isinstance(args, NCP.NCP_Args)
+        input_data = self._env.full_data()
+        input_norm = tf.norm(input_data)
+        shape = input_data.get_shape().as_list()
+        size = np.prod(shape)
+        order = len(shape)
+
+        with tf.name_scope('random-initial') as scope:
+            # TODO : best way to initialize is normally distributed pseudorandom numbers
+            #A = [tf.Variable(tf.nn.relu(tf.random_normal([shape[ii], args.rank])), name='A-%d' % ii) for ii in range(order)]
+            Uinit = rand_list(shape, args.rank)
+            A = [tf.Variable(Uinit[ii], name='A-%d' % ii) for ii in range(order)]
+            Am = A
+            A0 = A
+            A_update_op = [None for _ in range(order)]
+            Am_update_op = [None for _ in range(order)]
+            A0_update_op = [None for _ in range(order)]
+            t0 = tf.Variable(1.0)
+            t = tf.Variable(1.0)
+            t_update_op = None
+            t0_update_op = None
+            wA = [tf.Variable(1.0) for _ in range(order)]
+            wA_update_op = [None for _ in range(order)]
+            L = [tf.Variable(1.0, name='gradientLipschitz-%d' % ii) for ii in range(order)]
+            L0 = L
+            L_update_op = [None for _ in range(order)]
+            L0_update_op = [None for _ in range(order)]
+
+        with tf.name_scope('normalize-initial') as scope:
+            norm_init_op = [None for _ in range(order)]
+            for mode in range(order):
+                norm_init_op[mode] = A[mode].assign(A[mode]/tf.norm(A[mode], ord='fro')*input_norm)
+        with tf.name_scope('unfold-all-mode') as scope:
+            mats = [ops.unfold(input_data, mode) for mode in range(order)]
+            sizeN = [int(size/In) for In in shape]
+
+
+        t_update_op = t.assign((1+tf.sqrt(1+4*tf.square(t0)))/2)
+        w = (t0 - 1)/t_update_op
+        for mode in range(order):
+            L0_update_op[mode] = L0[mode].assign(L[mode])
+            if mode != 0:
+                with tf.control_dependencies([A_update_op[mode - 1]]):
+                    AtA = [tf.matmul(A[ii], A[ii], transpose_a=True, name='AtA-%d-%d' % (mode, ii)) for ii in range(order)]
+            else:
+                AtA = [tf.matmul(A[ii], A[ii], transpose_a=True, name='AtA-%d-%d' % (mode, ii)) for ii in range(order)]
+            V = ops.hadamard(AtA, skip_matrices_index=mode)
+            L_update_op[mode] = L[mode].assign(tf.norm(V))
+            XA = tf.matmul(mats[mode], ops.khatri(A, mode, True), name='XA-%d' % mode)
+            Gn = tf.subtract(tf.matmul(Am[mode], V), XA, name='G-%d' % mode)
+            A_update_op[mode] = A[mode].assign(tf.nn.relu(tf.subtract(Am[mode], tf.div(Gn, L_update_op[mode]))))
+            wA_update_op[mode] = wA[mode].assign(tf.minimum(w, tf.sqrt(L0_update_op[mode]/L_update_op[mode])))
+            Am_update_op[mode] = Am[mode].assign(A_update_op[mode] + tf.matmul(wA, A_update_op[mode]-A0[mode]))
+            A0_update_op[mode] = A0[mode].assign(A_update_op[mode])
+
+        t0_update_op = t0.assign(t)
+
+        with tf.name_scope('full-tensor') as scope:
+            P = KTensor(assign_op)
+            full_op = P.extract()
+        with tf.name_scope('loss') as scope:
+            loss_op = rmse_ignore_zero(input_data, full_op)
+
+
+
+
+
+
+
+
 
 
 
