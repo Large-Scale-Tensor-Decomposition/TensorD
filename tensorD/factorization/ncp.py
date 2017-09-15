b332459de78c3519f9cc159f8f0bfb62d51ca6ab
Shiloh Leung <zszx0259@me.com>
2017-09-16 02:36:27 +0800

    finish ncp & ncp_test

--- tensorD/factorization/ncp.py
+++ tensorD/factorization/ncp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2017/8/16 AM11:48
+# @Time    : 2017/9/15 PM10:52
 # @Author  : Shiloh Leung
 # @Site    : 
 # @File    : ncp.py
@@ -15,12 +15,14 @@ from tensorD.base import *
 from .factorization import *
 from .env import *
 
+
 class NCP(BaseFact):
     class NCP_Args(object):
-        def __init__(self, rank, validation_internal=-1, verbose=False):
+        def __init__(self, rank, validation_internal=-1, verbose=False, tol=1.0e-4):
             self.rank = rank
             self.validation_internal = validation_internal
             self.verbose = verbose
+            self.tol = tol
 
     def __init__(self, env):
         assert isinstance(env, Environment)
@@ -67,23 +69,20 @@ class NCP(BaseFact):
         order = len(shape)
 
         with tf.name_scope('random-initial') as scope:
-            # TODO : best way to initialize is normally distributed pseudorandom numbers
-            #A = [tf.Variable(tf.nn.relu(tf.random_normal([shape[ii], args.rank])), name='A-%d' % ii) for ii in range(order)]
-            Uinit = rand_list(shape, args.rank)
-            A = [tf.Variable(Uinit[ii], name='A-%d' % ii) for ii in range(order)]
+            # initialize with normally distributed pseudorandom numbers
+            A = [tf.Variable(rand(shape[ii], args.rank), name='A-%d' % ii, dtype=tf.float64) for ii in range(order)]
             Am = [tf.Variable(np.zeros(shape=(shape[ii], args.rank)), dtype=tf.float64) for ii in range(order)]
             A0 = [tf.Variable(np.zeros(shape=(shape[ii], args.rank)), dtype=tf.float64) for ii in range(order)]
             Am_init_op = [None for _ in range(order)]
             A0_init_op = [None for _ in range(order)]
             A_update_op = [None for _ in range(order)]
-            Am_update_op = [None for _ in range(order)]
-            A0_update_op = [None for _ in range(order)]
+            Am_update_op1 = [None for _ in range(order)]
+            Am_update_op2 = [None for _ in range(order)]
+            A0_update_op1 = [None for _ in range(order)]
             t0 = tf.Variable(1.0, dtype=tf.float64)
             t = tf.Variable(1.0, dtype=tf.float64)
-            t_update_op = None
-            t0_update_op = None
             wA = [tf.Variable(1.0, dtype=tf.float64) for _ in range(order)]
-            wA_update_op = [None for _ in range(order)]
+            wA_update_op1 = [None for _ in range(order)]
             L = [tf.Variable(1.0, name='gradientLipschitz-%d' % ii, dtype=tf.float64) for ii in range(order)]
             L0 = [tf.Variable(1.0, dtype=tf.float64) for _ in range(order)]
             L_update_op = [None for _ in range(order)]
@@ -92,42 +91,49 @@ class NCP(BaseFact):
         with tf.name_scope('normalize-initial') as scope:
             norm_init_op = [None for _ in range(order)]
             for mode in range(order):
-                norm_init_op[mode] = A[mode].assign(A[mode]/tf.norm(A[mode], ord='fro', axis=(0, 1))*tf.pow(input_norm, 1/order))
+                norm_init_op[mode] = A[mode].assign(
+                    A[mode] / tf.norm(A[mode], ord='fro', axis=(0, 1)) * tf.pow(input_norm, 1 / order))
                 A0_init_op[mode] = A0[mode].assign(norm_init_op[mode])
                 Am_init_op[mode] = Am[mode].assign(norm_init_op[mode])
         with tf.name_scope('unfold-all-mode') as scope:
             mats = [ops.unfold(input_data, mode) for mode in range(order)]
 
-
-        t_update_op = t.assign((1+tf.sqrt(1+4*tf.square(t0)))/2)
-        w = (t0 - 1)/t_update_op
         for mode in range(order):
 
             if mode != 0:
                 with tf.control_dependencies([A_update_op[mode - 1]]):
-                    AtA = [tf.matmul(A[ii], A[ii], transpose_a=True, name='AtA-%d-%d' % (mode, ii)) for ii in range(order)]
+                    AtA = [tf.matmul(A[ii], A[ii], transpose_a=True, name='AtA-%d-%d' % (mode, ii)) for ii in
+                           range(order)]
                     XA = tf.matmul(mats[mode], ops.khatri(A, mode, True), name='XA-%d' % mode)
             else:
                 AtA = [tf.matmul(A[ii], A[ii], transpose_a=True, name='AtA-%d-%d' % (mode, ii)) for ii in range(order)]
                 XA = tf.matmul(mats[mode], ops.khatri(A, mode, True), name='XA-%d' % mode)
             V = ops.hadamard(AtA, skip_matrices_index=mode)
             L0_update_op[mode] = L0[mode].assign(L[mode])
-            L_update_op[mode] = L[mode].assign(tf.reduce_max(tf.svd(V, compute_uv=False)))
+            with tf.control_dependencies([L0_update_op[mode]]):
+                L_update_op[mode] = L[mode].assign(tf.reduce_max(tf.svd(V, compute_uv=False)))
             Gn = tf.subtract(tf.matmul(Am[mode], V), XA, name='G-%d' % mode)
             A_update_op[mode] = A[mode].assign(tf.nn.relu(tf.subtract(Am[mode], tf.div(Gn, L_update_op[mode]))))
-            wA_update_op[mode] = wA[mode].assign(tf.minimum(w, tf.sqrt(L0_update_op[mode]/L_update_op[mode])))
-            Am_update_op[mode] = Am[mode].assign(A_update_op[mode] + wA_update_op[mode]*(A_update_op[mode]-A0[mode]))
-            with tf.control_dependencies([Am_update_op[mode]]):
-                # add control dependencies to avoid random computation sequence
-                A0_update_op[mode] = A0[mode].assign(A_update_op[mode])
-
-        t0_update_op = t0.assign(t_update_op)
 
         with tf.name_scope('full-tensor') as scope:
             P = KTensor(A_update_op)
             full_op = P.extract()
         with tf.name_scope('loss') as scope:
             loss_op = rmse_ignore_zero(input_data, full_op)
+
+        t_update_op = t.assign((1 + tf.sqrt(1 + 4 * tf.square(t0))) / 2)
+        w = (t0 - 1) / t
+        for mode in range(order):
+            # if RMSE loss is increasing
+            Am_update_op2[mode] = Am[mode].assign(A0[mode])
+            # if RMSE loss is not increasing
+            wA_update_op1[mode] = wA[mode].assign(tf.minimum(w, tf.sqrt(L0[mode] / L[mode])))
+            Am_update_op1[mode] = Am[mode].assign(A[mode] + wA_update_op1[mode] * (A[mode] - A0[mode]))
+            with tf.control_dependencies([Am_update_op1[mode]]):
+                A0_update_op1[mode] = A0[mode].assign(A[mode])
+
+        with tf.control_dependencies([Am_update_op1[order - 1]]):
+            t0_update_op1 = t0.assign(t)
 
         tf.summary.scalar('loss', loss_op)
 
@@ -136,12 +142,12 @@ class NCP(BaseFact):
         self._args = args
         self._init_op = init_op
         self._other_init_op = tf.group(*norm_init_op, *Am_init_op, *A0_init_op)
-        self._train_op = tf.group(*L_update_op, *L0_update_op, t_update_op, t0_update_op, *Am_update_op, *wA_update_op, *A0_update_op)
+        self._train_op = tf.group(*L_update_op, *L0_update_op, t_update_op)
+        self._train_op1 = tf.group(*wA_update_op1, *Am_update_op1, *A0_update_op1, t0_update_op1)
+        self._train_op2 = tf.group(*Am_update_op2)
         self._factor_update_op = A_update_op
         self._full_op = full_op
         self._loss_op = loss_op
-
-
 
     def train(self, steps):
         self._is_train_finish = False
@@ -153,6 +159,8 @@ class NCP(BaseFact):
         other_init_op = self._other_init_op
         factor_update_op = self._factor_update_op
         train_op = self._train_op
+        train_op1 = self._train_op1
+        train_op2 = self._train_op2
         full_op = self._full_op
         loss_op = self._loss_op
 
@@ -164,23 +172,33 @@ class NCP(BaseFact):
         print('Non-Negative CP model initial finish')
 
         for step in range(1, steps + 1):
-            if (step == steps) or (args.verbose) or (step == 1) or (step % args.validation_internal == 0 and args.validation_internal != -1):
-                self._factors, self._full_tensor, loss_v, sum_msg,  _ = sess.run([factor_update_op, full_op, loss_op, sum_op, train_op])
+            if (step == steps) or (args.verbose) or (step == 1) or (
+                                step % args.validation_internal == 0 and args.validation_internal != -1):
+                self._factors, self._full_tensor, loss_v, sum_msg, _ = sess.run(
+                    [factor_update_op, full_op, loss_op, sum_op, train_op])
                 sum_writer.add_summary(sum_msg, step)
-                print('step=%d, RMSE=%f' % (step, loss_v))
+                print('step=%d, RMSE=%.5f' % (step, loss_v))
             else:
-                self._factors, _ = sess.run([factor_update_op, train_op])
+                self._factors, loss_v, _ = sess.run([factor_update_op, loss_op, train_op])
+
+            if step == 1:
+                loss_v0 = loss_v + 1
+            crit = abs(loss_v - loss_v0) / (loss_v0 + 1) < args.tol
+
+            if loss_v < loss_v0:
+                sess.run(train_op1)
+                loss_v0 = loss_v
+            else:
+                sess.run(train_op2)
+
+            if crit:
+                nstall = nstall + 1
+            else:
+                nstall = 0
+            if nstall >= 3:
+                break
+
         self._lambdas = np.ones(shape=(1, args.rank))
 
-        print('Non-Negative CP model train finish, with RMSE = %.10f' % (loss_v))
+        print('Non-Negative CP model train finish, in %d steps, with RMSE = %.10f' % (step, loss_v))
         self._is_train_finish = True
-
-
-
-
-
-
-
-
-
-
--- tensorD/test/ncp_test.py
+++ tensorD/test/ncp_test.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time    : 2017/9/12 PM7:57
+# @Time    : 2017/9/15 PM10:53
 # @Author  : Shiloh Leung
 # @Site    : 
 # @File    : ncp_test.py
 # @Software: PyCharm Community Edition
-
 from tensorD.factorization.env import Environment
 from tensorD.factorization.ncp import NCP
 from tensorD.dataproc.provider import Provider
@@ -15,12 +14,11 @@ from tensorD.DataBag import *
 
 if __name__ == '__main__':
     data_provider = Provider()
-    X = gen_test_tensor([3, 4, 5], 3)
+    X = np.arange(24).reshape(3, 4, 2)
     data_provider.full_tensor = lambda: tf.constant(X, dtype=tf.float64)
     env = Environment(data_provider, summary_path='/tmp/tensord')
     ncp = NCP(env)
-    args = NCP.NCP_Args(rank=2, validation_internal=5)
+    args = NCP.NCP_Args(rank=2, validation_internal=1)
     ncp.build_model(args)
-    ncp.train(210)
+    ncp.train(200)
     print(ncp.full - X)
-
